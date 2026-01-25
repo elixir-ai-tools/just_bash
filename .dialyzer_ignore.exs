33cdaa907f461696c398f50f8a695c8d6a630bec
Ivar Vong <ivar@ivarvong.com>
2026-01-25 03:43:38 +0000

    Add AWK type spec issues to dialyzer ignore list

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -6,5 +6,9 @@
   {"lib/just_bash/commands/continue.ex", :callback_type_mismatch},
   {"lib/just_bash/commands/return.ex", :callback_type_mismatch},
   # Printf pattern match issue
-  {"lib/just_bash/commands/printf.ex", :pattern_match}
+  {"lib/just_bash/commands/printf.ex", :pattern_match},
+  # AWK parser/evaluator type specs use internal types
+  {"lib/just_bash/commands/awk/evaluator.ex", :invalid_contract},
+  {"lib/just_bash/commands/awk/evaluator.ex", :unknown_type},
+  {"lib/just_bash/commands/awk/parser.ex", :unknown_type}
 ]
